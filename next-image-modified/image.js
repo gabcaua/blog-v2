b8f34d3a6c670008b7c81f27b8f474a1c10799c7
gabcaua <cgs.brasil@outlook.com>
2023-08-15 15:00:33 -0300

    remove blur

--- next-image-modified/image.js
+++ next-image-modified/image.js
@@ -262,7 +262,7 @@ function Image(_param) {
     const blurStyle = placeholder === 'blur' && !blurComplete ? {
         backgroundSize: objectFit || 'cover',
         backgroundPosition: objectPosition || '0% 0%',
-        filter: 'blur(5px)',
+        filter: 'blur(0px)',
         backgroundImage: `url("${blurDataURL}")`
     } : {};
     if (layout === 'fill') {
